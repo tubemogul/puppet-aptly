14e01b518186db8e5902a2b56090b2d6323bb819
Joseph Herlant <herlantj@gmail.com>
2016-07-19 13:45:13 -0700

    Fixing and addig specs for the aptly_mirror type

--- lib/puppet/type/aptly_mirror.rb
+++ lib/puppet/type/aptly_mirror.rb
@@ -6,34 +6,35 @@ Puppet::Type.newtype(:aptly_mirror) do
 
   ensurable
 
-  newparam(:name, namevar: true) do
+  newparam(:name, :namevar => true) do
     desc "The name of the Aptly mirror. Example : debian-main"
   end
 
   newparam(:force, :boolean => true, :parent => Puppet::Parameter::Boolean) do
     desc "Force to delete the Apt mirror"
-    defaultto true
+    defaultto :true
   end
 
   newparam(:update, :boolean => true, :parent => Puppet::Parameter::Boolean) do
     desc "Automatically update the Apt mirror after the creation"
-    defaultto true
+    defaultto :true
   end
 
   newparam(:location) do
     desc "The URL for the source Debian repository"
     validate do |value|
       unless value.instance_of? String
-        raise ArgumentError , "%s is not a valid String" % value
+        raise ArgumentError , "%s is not a valid location (should be a string)" % value
       end
     end
+    defaultto :undef
   end
 
   newparam(:distribution) do
-    desc ""
+    desc "The distribution to mirror"
     validate do |value|
       unless value.instance_of? String
-        raise ArgumentError , "%s is not a valid String" % value
+        raise ArgumentError , "%s is not a valid distribution (should be a string)" % value
       end
     end
     defaultto :undef
@@ -45,20 +46,18 @@ Puppet::Type.newtype(:aptly_mirror) do
   end
 
   newparam(:architectures, :array_matching => :all) do
-    desc ""
+    desc "List of architectures to mirror"
     defaultto :undef
   end
 
-  newparam(:with_sources) do
-    desc "Mirror Sources"
+  newparam(:with_sources, :boolean => true, :parent => Puppet::Parameter::Boolean) do
+    desc "Whether to mirror the source packages or not"
     defaultto :false
-    newvalues(:true, :false)
   end
 
-  newparam(:with_udebs) do
-    desc "Download .udeb packages"
+  newparam(:with_udebs, :boolean => true, :parent => Puppet::Parameter::Boolean) do
+    desc "Whether to download .udeb packages or not"
     defaultto :false
-    newvalues(:true, :false)
   end
 
 end
