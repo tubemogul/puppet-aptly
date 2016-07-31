15835a1665a50d16e3f775425c1e5ec410778172
Joseph Herlant <herlantj@gmail.com>
2016-07-30 17:46:16 -0700

    Adding specs for the mirror, snapshot and publish defines

---
+++ spec/defines/snapshot_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'aptly::snapshot' do
+  context 'basic snapshot' do
+    let(:title) { '2016-07-30-daily' }
+    let(:params) {{
+      :source_type => 'repository',
+      :source_name => 'debian-main',
+    }}
+
+    it 'should call the aptly_snapshot provider' do
+      should contain_aptly_snapshot('2016-07-30-daily')\
+        .with_ensure('present')\
+        .with_source_type('repository')\
+        .with_source_name('debian-main')
+    end
+  end
+end
